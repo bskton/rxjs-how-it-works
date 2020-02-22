092e469efce7f581cbd0279b536815920f7083e9
Ilya Levin <bskton@gmail.com>
2020-02-22 20:16:22 +1000

    Hello RxJS script and webpack config

---
+++ README.md
@@ -0,0 +1,17 @@
+# RxJS: Understaning How It Works
+
+The starter project for RxJS tutorial.
+
+## Requirements
+
+Node.js 13.8.0+
+Yarn 1.21.1+
+
+## Usage
+
+```
+git clone https://github.com/bskton/rxjs-how-it-works.git
+cd rxjs-how-it-works
+yarn install
+yarn run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'RxJS: Understanding How It Works'
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
