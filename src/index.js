893379a6277c44ecb33850a853178530cd2ecfb3
Ilya Levin <bskton@gmail.com>
2020-02-23 23:35:27 +1000

    Simple implementation of observable and timer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello RxJS!');
+class Observable {
+    constructor(subscriptionFunction) {
+        this.subscriptionFunction = subscriptionFunction;
+    }
+
+    subscribe(observer) {
+        return this.subscriptionFunction(observer);
+    }
+}
+
+function timer(seconds) {
+    const subscriptionFunction = observer => {
+        const timeoutId = setTimeout(() => {
+            observer.next('Cookie is ready!');
+        }, seconds * 1000);
+
+        return () => clearTimeout(timeoutId);
+    }
+
+    return new Observable(subscriptionFunction);
+}
+
+const myOvenTimer = timer(3);
+
+myOvenTimer.subscribe({
+    next: value => console.log(value)
+});
